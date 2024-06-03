457f5b03a9b7d2bb14004c45851331dd47255479
Luan Henrique <luanhenrique.dev@gmail.com>
2024-06-03 07:22:00 -0300

    fix regex

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -24,15 +24,15 @@ document.addEventListener('DOMContentLoaded', function () {
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions
 //https://www.regular-expressions.info/
 function aplicarMascaraRG(input) {
-    input.value = input.value.replace(/\D/g, '').replace(/(\d{2})(\d{3})(\d{3})(\d{1})/, '$1.$2.$3-$4');
+    input.value = input.value.replace(/\D/g, '').replace(/^(\d{2})(\d{3})(\d{3})(\d{1}).*/, '$1.$2.$3-$4');
 }
 
 function aplicarMascaraCEP(value) {
-    return value.replace(/\D/g, '').replace(/(\d{5})(\d)/, '$1-$2');
+    return value.replace(/\D/g, '').replace(/^(\d{5})(\d{3}).*/, '$1-$2');
 }
 
 function aplicarMascaraCelular(value) {
-    return value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '($1) $2').replace(/(\d{5})(\d)/, '$1-$2');
+    return value.replace(/\D/g, '').replace(/^(\d{2})(\d{4,5})(\d{4}).*/, '($1) $2-$3');
 }
 
 function aplicarMascaraDocumento(input, tipoDocumento) {
@@ -44,5 +44,5 @@ function aplicarMascaraDocumento(input, tipoDocumento) {
 }
 
 function aplicarMascaraCPF(value) {
-    return value.replace(/\D/g, '').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    return value.replace(/\D/g, '').replace(/^(\d{3})(\d{3})(\d{3})(\d{2}).*/, '$1.$2.$3-$4');
 }
